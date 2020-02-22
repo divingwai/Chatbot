8aa35cf6340aef299abbdb3b8dabbab822086cc3
divingwai <divingwai@hotmail.com>
2020-02-22 19:42:30 +0800

    Initial check-in of module Chatbot.Master

---
+++ Chatbot.Execute/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chatbot.Execute
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
